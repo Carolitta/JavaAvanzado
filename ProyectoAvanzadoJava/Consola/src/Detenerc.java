24b5ade5da47c798e05d7d7de5617bd120a7cb5d
Carolitta <daylic.fi@gmail.com>
2018-10-28 14:27:03 -0500

    :milky_way: subiendo proyecto

---
+++ ProyectoAvanzadoJava/Consola/src/Detenerc.java
@@ -0,0 +1,17 @@
+
+import java.io.Serializable;
+/*
+ *Programa realizado por 
+ *Aguilar Cabello Héctor Eduardo Prebe 2
+ *Rodríguez Dávalos Carolina Prebe 20
+ *Equipo buddys
+ */
+
+/**
+ *
+ * @author hector
+ * @author carolitta
+ */
+public class Detenerc implements Serializable{
+    public Boolean parar = false;
+}
